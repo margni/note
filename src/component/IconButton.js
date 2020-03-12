6d9168804dfe50e49f71053c42bc4611d7f009b0
Stephen Ingram <stephen@margni.com>
2020-03-12 18:10:22 +0000

    Change secondary icon button colour, fix last note in list has border, remove iconbutton key

--- src/component/IconButton.js
+++ src/component/IconButton.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 import './IconButton.css';
 
-// key is used to force re-render for css animation.
 export const IconButton = ({name, onClick, size, position, secondary}) =>
     <button
         className={'IconButton icon-' + name + (size === 'large' ? ' IconButton--large' : '') + (position === 'right' ? ' IconButton--right' : '') + (secondary ? ' IconButton--secondary' : '')}
-        key={name}
         onClick={onClick}
     ></button>;
