3ad24654e1c7d79182392a2a1390f515943d52a8
Stephen Ingram <stephen@margni.com>
2020-09-26 11:38:58 +0100

    Fixed broken unit test

--- src/component/Main.test.js
+++ src/component/Main.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import { AuthContext } from '../context/AuthContext';
+
 import { Main } from './Main';
 
 test('renders', () => {
-    render(<Main />);
+    const wrapper = ({ children }) => (
+        <AuthContext.Provider value={{ user: {} }}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+    render(<Main />, { wrapper });
 });
