7f8d7d971cb2b051d0e07a61a86a3403359e81f1
Stephen Ingram <stephen@margni.com>
2020-09-28 20:26:34 +0100

    Remove any deferred install prompt on appinstalled

--- src/context/InstallContext.js
+++ src/context/InstallContext.js
@@ -1,27 +1,30 @@
 import React, { useContext, useEffect, useState } from 'react';
 
-window.addEventListener(
-    'beforeinstallprompt',
-    (event) => console.log(event),
-    false
-);
-
 export const InstallContext = React.createContext(null);
 
 export const InstallProvider = ({ children }) => {
     const [deferredPrompt, setDeferredPrompt] = useState();
 
     useEffect(() => {
-        const handler = (e) => {
+        const onBeforeInstallPrompt = (e) => {
             e.preventDefault();
 
             setDeferredPrompt(e);
         };
 
-        window.addEventListener('beforeinstallprompt', handler, false);
+        const onAppInstalled = () => {
+            setDeferredPrompt();
+        };
+
+        window.addEventListener('beforeinstallprompt', onBeforeInstallPrompt);
+        window.addEventListener('appinstalled', onAppInstalled);
 
         return () => {
-            window.removeEventListener('beforeinstallprompt', handler);
+            window.removeEventListener(
+                'beforeinstallprompt',
+                onBeforeInstallPrompt
+            );
+            window.removeEventListener('appinstalled', onAppInstalled);
         };
     }, []);
 
