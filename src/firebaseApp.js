c8db6e4be0e805f6b8d001ae512743d49b8e609c
Steve I <1615137+jixor@users.noreply.github.com>
2022-03-04 01:14:38 +0000

    Upgrade; firebase v9, react-scripts v5, new service worker, etc.

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/analytics';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/analytics';
 
 // Initialize Firebase
 export const firebaseApp = firebase.initializeApp({
